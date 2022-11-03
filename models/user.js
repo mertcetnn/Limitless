e320d2a6815a935162de705b6a51d5c9c441c542
MertCetin <mert.cetnn@gmail.com>
2022-11-03 00:43:51 -0400

    user model d

---
+++ models/user.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  usurname: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/],
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Thought",
+    },
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+
+  toJSON: {
+    virtuals: true,
+  },
+  id: false,
+});
+
+// get total count of friends
+UsersSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+// create the Users model using the Users Schema
+const Users = model("Users", UsersSchema);
+
+// Export Users module
+module.exports = Users;
